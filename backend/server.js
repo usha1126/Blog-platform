888607a1f8978fb23184b051a0d9d3a8982e32c3
usha1126 <upparausharani2002@gmail.com>
2026-03-17 13:13:04 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Serve static files from frontend folder
-app.use(express.static(path.join(__dirname, 'frontend')));
+app.use(express.static(path.join(__dirname, '../frontend')));
+
+
 
 // MongoDB connection
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/blog-platform', {
